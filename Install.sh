1dbb755ff2905b2af439d593ffbb9f40e36ee366
Mario Spoerl <m.spoerl@mailbox.org>
2022-04-25 19:33:47 +0200

    Finished first version of the install script

--- Install.sh
+++ Install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Update the system
+sudo dnf update
+
 # Install Login Manager SDDM
 sudo dnf install sddm
 
@@ -21,4 +24,28 @@ cd ~/GITREPOS/fedora-evo-edition
 
 cp -ivr ~/GITREPOS/fedora-evo-edition/* ~/
 
-# 
+
+# Install the base packages
+sudo dnf install bspwm picom kitty firefox rofi sxhkd vim thunar dconf-editor arandr fontawesome-fonts fontawesome-fonts-web nitrogen
+
+
+# Install my extra fonts
+cd ~/Downloads
+
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip
+
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Meslo.zip
+
+mkdir -v ~/.fonts
+
+mv ~/Downloads/FiraCode.zip ~/.fonts
+mv ~/Downloads/Meslo.zip ~/.fonts
+
+cd ~/.fonts
+
+unzip FiraCode.zip
+unzip Meslo.zip
+
+
+# Install all my software
+sudo dnf install keepassxc
--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # fedora-evo-edition
 My Linux fedora configuration
 
-## Prerequisite
+## Prerequisites
 - Base is a Fedora Server 36 Beta Installation - Select Custom Fedora OS with the following groups: Standard, Audio & Video and Window Manager
 
-
+## Installation
+- Run the Install.sh Script to install the desktop and the other software 
